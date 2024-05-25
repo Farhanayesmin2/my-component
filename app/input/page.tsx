500bacd8cd30e3449b4e96cd52606acf53f2cd8b
Mst Farhana Yesmin <108618765+Farhanayesmin2@users.noreply.github.com>
2024-05-25 10:16:04 +0600

    create the page

---
+++ app/input/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "@/components/primitives";
+
+export default function InputPage() {
+  return (
+    <div>
+      <h1 className={title()}>Input</h1>
+      <h3>Here is a input page</h3>
+    </div>
+  );
+}
