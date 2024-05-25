c677037f709c15534ebd629d1c5427a41c35fc57
Mst Farhana Yesmin <108618765+Farhanayesmin2@users.noreply.github.com>
2024-05-25 11:30:45 +0600

    fix the length

--- app/input/page.tsx
+++ app/input/page.tsx
@@ -13,39 +13,36 @@ export default function InputPage() {
   };
 
   return (
-    <>
-      {/* This section is create for upload input */}
-      <section className="container mx-auto">
-        <div className="flex flex-col items-center justify-center py-4 m-24 border-[#3db78a] border-2 border-dashed hover:border-dotted">
-          <p className="text-2xl text-center text-gray-700 font-bold">Upload</p>
-          <label className="flex flex-col items-center bg-white rounded-lg tracking-wide uppercase cursor-pointer hover:text-white ease-linear transition-all duration-150">
-            <Player
-              autoplay
-              loop
-              src={uploadIcon}
-              style={{ height: '200px', width: '200px' }}
-            >
-              <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-            </Player>
-            <input type="file" className="hidden" onChange={handleFileAdd} multiple />
-          </label>
-          <p className="mt-4 text-center text-gray-700">
-            Drag and Drop or <span className="text-[#3db78a]">Choose file</span> to upload <br />
-            PDF, DOCX, DOC, PPTX or PPT
-          </p>
-        </div>
-        <div className="mt-4">
-          <h4 className="text-xl font-bold text-gray-700">Uploaded Files:</h4>
-          <ul>
-            {files.map((file, index) => (
-              <li key={index} className="text-gray-700">
-                {file.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
-      <h3>Here is a input page</h3>
-    </>
+    <div >
+      {/* This section is created for upload input */}
+      <div className="flex flex-col items-center justify-center w-full py-4 border-[#3db78a] border-2 border-dashed hover:border-dotted">
+        <p className="text-2xl text-center text-gray-700 font-bold">Upload</p>
+        <label className="flex flex-col items-center bg-white rounded-lg tracking-wide uppercase cursor-pointer hover:text-white ease-linear transition-all duration-150">
+          <Player
+            autoplay
+            loop
+            src={uploadIcon}
+            style={{ height: '200px', width: '200px' }}
+          >
+            <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+          </Player>
+          <input type="file" className="hidden" onChange={handleFileAdd} multiple />
+        </label>
+        <p className="mt-4 text-center text-gray-700">
+          Drag and Drop or <span className="text-[#3db78a]">Choose file</span> to upload <br />
+          PDF, DOCX, DOC, PPTX or PPT
+        </p>
+      </div>
+      <div className="mt-4">
+        <h4 className="text-xl font-bold text-gray-700">Uploaded Files:</h4>
+        <ul>
+          {files.map((file, index) => (
+            <li key={index} className="text-gray-700">
+              {file.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
