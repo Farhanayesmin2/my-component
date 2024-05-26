c4dd75704ef04b6ca3e4ec1f42d55cc7da6ef26c
Mst Farhana Yesmin <108618765+Farhanayesmin2@users.noreply.github.com>
2024-05-27 00:18:25 +0600

    change the color in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,16 +20,16 @@ const LoginPage = () => {
 
   return (
     <div className="flex items-center justify-center h-screen font-sans">
-      <div className="bg-white border-2 border-r-teal-400 rounded-3xl shadow-xl w-full max-w-[768px] h-[480px] relative overflow-hidden">
+      <div className="bg-white border-2 border-r-emerald-500 rounded-3xl shadow-xl w-full max-w-[768px] h-[480px] relative overflow-hidden">
         <div className="grid grid-cols-1 md:grid-cols-2 h-full">
-          <div className="hidden md:flex items-center justify-center rounded-full bg-teal-500 text-white rounded-r-[40%] rounded-l-3xl h-full">
+          <div className="hidden md:flex items-center justify-center rounded-full bg-emerald-500 text-white rounded-r-[40%] rounded-l-3xl h-full">
             <div className="flex flex-col items-center p-6 text-center">
               <h1 className="text-2xl font-bold mb-4">Welcome Back!</h1>
               <p className="mb-4">
                 Enter your personal details to use all of our site features
               </p>
               <NextLink href="/signup">
-                <button className="bg-white text-teal-500 py-2 px-4 rounded-full">
+                <button className="bg-white text-emerald-500 py-2 px-4 rounded-full">
                   SignUp
                 </button>
               </NextLink>
@@ -37,7 +37,7 @@ const LoginPage = () => {
           </div>
           <div className="p-6 flex flex-col items-center justify-center h-full">
             <form className="flex flex-col items-center w-full">
-              <h1 className="text-2xl font-bold mb-4 text-teal-600">
+              <h1 className="text-2xl font-bold mb-4 text-emerald-500">
                 Login Account
               </h1>
               <span className="text-gray-600 mb-4">
@@ -46,13 +46,13 @@ const LoginPage = () => {
               <div className="flex space-x-4 mb-4">
                 <a
                   href="www.google.com"
-                  className="border shadow-md shadow-teal-600 border-teal-300 rounded-full flex items-center justify-center w-10 h-10 text-gray-500"
+                  className="border shadow-md shadow-emerald-400 border-emerald-300 rounded-full flex items-center justify-center w-10 h-10 text-gray-500"
                 >
                   <FaGooglePlusG />
                 </a>
                 <a
                   href="www.facebook.com"
-                  className="border shadow-md shadow-teal-600 border-teal-300 rounded-full flex items-center justify-center w-10 h-10 text-gray-500"
+                  className="border shadow-md shadow-emerald-400 border-emerald-300 rounded-full flex items-center justify-center w-10 h-10 text-gray-500"
                 >
                   <FaFacebookF />
                 </a>
@@ -88,9 +88,9 @@ const LoginPage = () => {
                     onClick={toggleVisibility}
                   >
                     {isVisible ? (
-                      <IoEye className="text-2xl text-teal-600 text-default-400 pointer-events-none" />
+                      <IoEye className="text-2xl text-emerald-600 text-default-400 pointer-events-none" />
                     ) : (
-                      <IoEyeOff className="text-2xl text-teal-500/80  text-default-400 pointer-events-none" />
+                      <IoEyeOff className="text-2xl text-emerald-500/80  text-default-400 pointer-events-none" />
                     )}
                   </button>
                 }
@@ -101,7 +101,7 @@ const LoginPage = () => {
                 <button
                   type="button"
                   title="Start Login"
-                  className="w-full py-3 px-6 text-center rounded-full transition bg-gradient-to-r from-teal-500 via-teal-100/100 to-teal-500 shadow-md sm:w-max"
+                  className="w-full py-3 px-6 text-center rounded-full transition bg-gradient-to-r from-emerald-500 via-teal-100/100 to-emerald-500 shadow-md sm:w-max"
                 >
                   <span className="block text-[#1c100c] font-semibold text-sm">
                     Login
