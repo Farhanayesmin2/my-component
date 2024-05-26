047600df1d96e9faac2f793ce14e1937ca86b76d
Mst Farhana Yesmin <108618765+Farhanayesmin2@users.noreply.github.com>
2024-05-26 23:47:49 +0600

    create te google icon

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import React from "react";
+import { FaGooglePlusG } from "react-icons/fa";
+
+// Inside the form
 
 const SignupPage = () => {
   return (
@@ -8,10 +11,21 @@ const SignupPage = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 h-full">
           <div className="p-6 flex flex-col items-center justify-center h-full">
             <form className="flex flex-col items-center w-full">
-              <h1 className="text-2xl font-bold mb-4 text-teal-500">Create Account</h1>
+              <h1 className="text-2xl font-bold mb-4 text-teal-500">
+                Create Account
+              </h1>
               <span className="text-gray-600 mb-4">
                 or use your email for registration
               </span>
+
+              <div className="flex space-x-4 mb-4">
+                <a
+                  href="#"
+                  className="border shadow-md shadow-teal-600 border-teal-300 rounded-full flex items-center justify-center w-10 h-10 text-gray-500"
+                >
+                  <FaGooglePlusG />
+                </a>
+              </div>
             </form>
           </div>
         </div>
